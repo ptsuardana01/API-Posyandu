d8f145c3bf27da26b456264897eedb83ca2325ca
ptsuardana01 <ptsuardana01@gmail.com>
2021-12-29 02:20:42 +0800

    sesudah pull

--- public/js/app.js
+++ public/js/app.js
@@ -6786,7 +6786,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "\n*[data-v-48cc2842] {\n    scroll-behavior: smooth;\n}\nbody[data-v-48cc2842] {\n    font-family: \"Nunito\", sans-serif;\n}\n", ""]);
+___CSS_LOADER_EXPORT___.push([module.id, "\n*[data-v-48cc2842] {\r\n    scroll-behavior: smooth;\n}\nbody[data-v-48cc2842] {\r\n    font-family: \"Nunito\", sans-serif;\n}\r\n", ""]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
